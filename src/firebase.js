03116d1278a1638be844711c0b7f89cce28973ba
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2020-04-13 10:20:43 +0530

    differently adding private keys  On branch master 	modified:   src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,30 +5,10 @@ if (process.env.NODE_ENV === 'development') {
 	serviceAccount = require('../config');
 } else {
 	let {
-		private_key_id,
-		project_id,
-		type,
-		private_key,
-		client_email,
-		client_id,
-		auth_uri,
-		token_uri,
-		auth_provider_x509_cert_url,
-		client_x509_cert_url,
+		FIREBASE_PRIVATE_KEY
     } = process.env;
     
-    serviceAccount = {
-        private_key_id,
-		project_id,
-		type,
-		private_key,
-		client_email,
-		client_id,
-		auth_uri,
-		token_uri,
-		auth_provider_x509_cert_url,
-		client_x509_cert_url,
-    };
+    serviceAccount = JSON.parse(FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'));
 }
 
 admin.initializeApp({
